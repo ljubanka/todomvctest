825a6b069aafc3e621c122464ea1ca62165cb7b8
ljubanka <lvpopl@gmail.com>
2016-05-06 15:58:52 +0300

    small changes

--- src/test/java/ua/net/itlabs/hw5/pagemodules/TodoMVCCompletedFilterTest.java
+++ src/test/java/ua/net/itlabs/hw5/pagemodules/TodoMVCCompletedFilterTest.java
@@ -1,7 +1,6 @@
 package ua.net.itlabs.hw5.pagemodules;
 
 import org.junit.Test;
-import ua.net.itlabs.hw5.pageobjects.pages.TodoMVCPage;
 
 import static ua.net.itlabs.hw5.pagemodules.pages.TodoMVC.*;
 import static ua.net.itlabs.hw5.pagemodules.pages.TodoMVC.TaskType.COMPLETED;
@@ -9,7 +8,6 @@ import static ua.net.itlabs.hw5.pagemodules.pages.TodoMVC.TaskType.COMPLETED;
 public class TodoMVCCompletedFilterTest {
     @Test
     public void testDelete() {
-        TodoMVCPage page = new TodoMVCPage();
         givenAtCompleted(COMPLETED, "1", "2");
 
         delete("2");
@@ -19,7 +17,6 @@ public class TodoMVCCompletedFilterTest {
 
     @Test
     public void testReopen() {
-        TodoMVCPage page = new TodoMVCPage();
         givenAtCompleted(COMPLETED, "1");
 
         toggle("1");
@@ -29,7 +26,6 @@ public class TodoMVCCompletedFilterTest {
 
     @Test
     public void testClearCompleted() {
-        TodoMVCPage page = new TodoMVCPage();
         givenAtCompleted(COMPLETED, "1");
 
         clearCompleted();
@@ -38,7 +34,6 @@ public class TodoMVCCompletedFilterTest {
 
     @Test
     public void testReopenAll() {
-        TodoMVCPage page = new TodoMVCPage();
         givenAtCompleted(COMPLETED, "1", "2");
 
         toggleAll();
